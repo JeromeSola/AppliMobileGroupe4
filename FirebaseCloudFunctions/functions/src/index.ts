ea8c630f73971e650f0f120286c9b82a68a09af2
Samy Ouyahia <ouyahia.samy@yahoo.fr>
2019-12-12 11:35:39 +0100

    Cloud function done

--- FirebaseCloudFunctions/functions/src/index.ts
+++ FirebaseCloudFunctions/functions/src/index.ts
@@ -1,29 +1,44 @@
 import * as functions from 'firebase-functions';
-const admin = require('firebase-admin');
-admin.initializeApp();
+
 const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
   projectId: 'coachman-2aaa8',
 });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.addUser = functions.https.onRequest(async (req, res) => {
+function getUsernameFromMail(gmail: string): string {
+  const reg = new RegExp('(.+)@(?:.+)', 'i');
+  const result = gmail.match(reg);
+  return (result === null) ? 'unknown' : result[1];
+}
 
-    const user={
-        mail: req.query.mail,
-        name: req.query.name,
-        family_name: req.query.family_name,
-        access_token:req.query.access_token
-    }
-    return db.collection('Users')
-      .add({user})
-      .then( (doc: any) => {
-        return res.status(200).send(doc);
-      }).catch((err: any) => {
-        console.error(err);
-        return res.status(404).send({ error: 'unable to store', err });
+export const onUserLogin = functions.https.onRequest((request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  const usersRef = db.collection('Users')
+  usersRef.get()
+  .then((docSnapshot: any) => {
+      let userInfo;
+      docSnapshot.forEach((doc: any) => {
+          if (doc._fieldsProto.gmail !== undefined && doc._fieldsProto.gmail.stringValue === request.query.gmail) { userInfo = doc.data(); }
       });
-  });
+      if (userInfo !== undefined) { response.status(208).send(userInfo); }
+      else {
+        const username = getUsernameFromMail(request.query.gmail);
+        const userDoc = {
+          gmail: request.query.gmail,
+          firstName: request.query.firstName,
+          lastName: request.query.lastName,
+          username: username,
+          access_token: request.query.access_token
+        }
+        db.collection('Users')
+        .add(userDoc)
+        .then((doc: any) => {
+            response.status(200).send(userDoc);
+        }).catch((err: any) => {
+            console.error(err);
+            return response.status(404).send({ error: 'unable to store', err });
+        });
+      }          
+  })
+});
