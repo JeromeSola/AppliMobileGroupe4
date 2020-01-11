c155e8c463c828211d79a20289ab3af067352248
rajaona-jean <rajaona@Isram>
2020-01-11 11:59:47 +0100

    ajout side bar, vue mes activités

--- FrontEnd/app-front/src/app/services/google-fit.service.ts
+++ FrontEnd/app-front/src/app/services/google-fit.service.ts
@@ -9,27 +9,53 @@ export class GoogleFitService {
 
   constructor(private http: HttpClient, private loginService: LoginService) { }
 
-  getData(){
+  getData() {
     this.http.get(`https://www.googleapis.com/fitness/v1/users/me/dataSources?alt=json&access_token=${this.loginService.loggedUser.access_token}`)
-    .subscribe(
-      (data: any) => {
-        console.log(data)
-      },
-      error => {
-        console.log(error)
-  
-      });
+      .subscribe(
+        (data: any) => {
+          console.log(data)
+        },
+        error => {
+          console.log(error)
+
+        });
   }
 
-  getData2(){
-    this.http.get(`https://www.googleapis.com/fitness/v1/users/me/sessions?alt=json&access_token=${this.loginService.loggedUser.access_token}`)
-    .subscribe(
-      (data: any) => {
-        console.log(data)
-      },
-      error => {
-        console.log(error)
-  
-      });
+  getData2() {
+    let data = {
+      aggregateBy: [
+        {
+          dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+        }
+      ],
+      endTimeMillis: 1578678205703,
+      startTimeMillis: 1574678205703,
+      bucketByTime: {
+        durationMillis: 84000000
+      }
+
+    }
+    this.http.post(`https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate?alt=json&access_token=${this.loginService.loggedUser.access_token}`, data)
+      .subscribe(
+        (data: any) => {
+          console.log(data)
+        },
+        error => {
+          console.log(error)
+
+        });
   }
+
+  // {
+  //   "aggregateBy": [
+  //     {
+  //       "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+  //     }
+  //   ],
+  //   "endTimeMillis": 1578678205703,
+  //   "startTimeMillis": 1574678205703,
+  //   "bucketByTime": {
+  //     "durationMillis": 84000000
+  //   }
+  // }
 }
